60c89347b5b6dd24eb4950ad43653216a7eaa076
Jazzmine07 <61366865+Jazzmine07@users.noreply.github.com>
2021-12-25 18:37:09 +0800

    Clinician landing page yieee

---
+++ app/src/main/java/com/capstone/capstone_group/isklinika/ActivityClinicianLanding.java
@@ -0,0 +1,16 @@
+package com.capstone.capstone_group.isklinika;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityClinicianLanding extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clinician_landing);
+    }
+}
