e91215d614cc49139ad20ade407827b373938f4e
deasisjhose <jhose_mhiguel_deasis@dlsu.edu.ph>
2022-01-10 22:48:54 +0800

    cccccc

--- app/src/main/java/com/capstone/capstone_group/isklinika/ActivityDiseaseSurveillance.java
+++ app/src/main/java/com/capstone/capstone_group/isklinika/ActivityDiseaseSurveillance.java
@@ -39,7 +39,7 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-
+//disease
 public class ActivityDiseaseSurveillance extends AppCompatActivity implements View.OnClickListener{
 
     private ArrayList<ClassStudentInfo> children ;
